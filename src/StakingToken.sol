ea790e0bfc2e9ebb5d9a51ade4854e3967538e2e
Rares Stanciu <rcstanciu@protonmail.com>
2025-08-26 16:50:07 +0300

    fix: solid

--- src/StakingToken.sol
+++ src/StakingToken.sol
@@ -2,10 +2,16 @@ pragma solidity ^0.8.25;
 
 contract StakingToken {
     uint256 totaltokens = 0;
+    address admin = 0x2c1b3672E178457C7533a7f4aB491a920d15e444;
 
     constructor() {}
 
     mapping (address => uint256) balances;
+
+    modifier onlyAdmin() {
+      require(msg.sender == admin, "not an admin");
+      _;
+    }
 
     function deposit() public payable returns (bool success) {
         balances[msg.sender] += msg.value;
@@ -15,19 +21,20 @@ contract StakingToken {
 
     function withdraw(uint256 _value) public returns (bool success) {
         if (balances[msg.sender] < _value) return false;
-        (bool success_, ) = msg.sender.call{value: _value}("");
-        require(success_, "transaction failed");
-
         balances[msg.sender] -= _value;
         totaltokens -= _value;
+
+        payable(msg.sender).transfer(_value);
+
         return true;
     }
 
-    /// Should be called only by admins to recover all funds
+    /// Should be called only by the admin, to recover all funds
     function adminWithdrawEverything() public returns (bool success) {
-        (bool success_, ) = msg.sender.call{value: totaltokens}("");
-        require(success_, "transaction failed");
         totaltokens = 0;
+
+        payable(msg.sender).transfer(totaltokens);
+
         return true;
     }
 
